76baa49bbf08cb3a20a86a1cdaed7e3e504fe24c
Esteban Sánchez <e.sanchez@globant.com>
2022-01-12 20:28:43 -0600

    added products module to container module

---
+++ container/webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new ModuleFederationPlugin({
+      name: 'app',
+      remotes: {
+        products: 'products@http://localhost:8081/remoteEntry.js',
+      },
+    }),
+  ],
+};
